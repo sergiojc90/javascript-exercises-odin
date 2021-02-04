46dc9edbf08b7f67de440fd16a2562360c907986
sergiojc90 <sergiojc90@gmail.com>
2021-02-03 19:52:16 -0600

    Finallygit add .

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverseString = "";
+    let largo = string.length;
+    
+    for(i=0 ; i < largo ; i++){
 
+        reverseString += string.charAt(string.length-1);
+
+        string = string.slice(0,-1);
+    }
+    return reverseString;
 }
 
 module.exports = reverseString
