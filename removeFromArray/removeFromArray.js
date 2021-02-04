27ffe378559e596002f6a0b41720fe9a86b2f7bf
sergiojc90 <sergiojc90@gmail.com>
2021-02-03 22:52:56 -0600

    Remember that you can increse or decrese the counter of a loop inside de code that it is looping

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(Array, ...values) {
 
+        let largoValues = values.length;
+        let largoArray =Array.length;
+
+        for (i=0; i<largoArray;i++){
+
+            for(j=0; j<largoValues;j++){
+                if(Array[i]===values[j]){
+                    let place = Array.indexOf(Array[i]);
+                    Array.splice(place,1);
+                    i--;
+                }
+            }
+        }
+
+        return Array;
 }
 
 module.exports = removeFromArray
