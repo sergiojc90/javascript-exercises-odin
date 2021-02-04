6f84f9f38b17322f89abafeb701214f7e681bd4d
sergiojc90 <sergiojc90@gmail.com>
2021-02-04 13:11:05 -0600

    Quite simple problem, just remember to check the formulas you are translating in to code

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(ftemp) {
+  let ctemp = (ftemp-32)*(5/9);
+  let rounded = Math.round(ctemp*10)/10;
+  
+  return rounded;
 }
 
-const ctof = function() {
+const ctof = function(ctemp) {
+  let ftemp = (ctemp*(9/5))+32;
+  let rounded = Math.round(ftemp*10)/10;
 
+  return rounded;
 }
 
 module.exports = {
