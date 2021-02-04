17b340ecd8aad5efdb5d837625b8f55cb7fe185f
sergiojc90 <sergiojc90@gmail.com>
2021-02-03 23:13:10 -0600

    Comparing the typeof variables can be used as a boolean argumetn

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+
+
+    if(a < 0 || b < 0 || typeof a != 'number' || typeof b != 'number'){
+
+        return "ERROR";
+
+    }else{
+        let sum = 0;
+
+        if (a > b){
+            let c = a;
+            a = b;
+            b = c; 
+        }
+        for (i = a; i <= b; i++){
+            sum += i;    
+        }
+        return sum;
+    }
 
 }
 
